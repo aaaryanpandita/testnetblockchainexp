9dd9d1da1022184076df5d62f7e4f68d8a4fd54a
Aryan8899 <aryanpandita003@gmail.com>
2026-07-17 15:08:35 +0530

    projectid

--- configs/app/features/blockchainInteraction.ts
+++ configs/app/features/blockchainInteraction.ts
@@ -4,7 +4,7 @@ import chain from '../chain';
 import { getEnvValue } from '../utils';
 import opSuperchain from './opSuperchain';
 
-const walletConnectProjectId = '46d3491db08a993aee5dc069f774f048';
+const walletConnectProjectId = 'bd682e2457633b8755ee89d61169f2a3';
 
 const title = 'Blockchain interaction (writing to contract, etc.)';
 
